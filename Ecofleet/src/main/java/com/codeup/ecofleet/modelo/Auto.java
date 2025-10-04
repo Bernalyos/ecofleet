6d5e2b3a120df920dcae0fcbb17d05d29bbba595
yoshira abrros <barrosyoshira@gmail.com>
2025-10-03 19:36:22 -0500

    feat:Ajustes en los modelos

--- Ecofleet/src/main/java/com/codeup/ecofleet/modelo/Auto.java
+++ Ecofleet/src/main/java/com/codeup/ecofleet/modelo/Auto.java
@@ -1,22 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+package com.codeup.ecofleet.modelo;
 
-/**
- *
- * @author Coder
- */
 public class Auto extends Vehiculo {
-    private int numeroPuertas;
+    private int puertas;
 
     public Auto() {}
 
-    public Auto(int id, String marca, String modelo, String placa, boolean disponible, int numeroPuertas) {
-        super(id, marca, modelo, placa, disponible);
-        this.numeroPuertas = numeroPuertas;
+    public Auto(int id, String marca, String modelo, String placa, boolean disponible, int puertas) {
+        // 👇 Aquí pasamos "AUTO" como tipo fijo
+        super(id, "AUTO", marca, modelo, placa, disponible);
+        this.puertas = puertas;
     }
 
-    public int getNumeroPuertas() { return numeroPuertas; }
-    public void setNumeroPuertas(int numeroPuertas) { this.numeroPuertas = numeroPuertas; }
+    public int getPuertas() { return puertas; }
+    public void setPuertas(int puertas) { this.puertas = puertas; }
 }
